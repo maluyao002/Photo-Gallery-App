106d9429cd3b7786897ebd0120288132254b223b
luyao ma <hakula@luyaos-MacBook-Pro.local>
2015-04-18 21:24:53 -0400

    Initial Commit

---
+++ Photo Gallery App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo Gallery App
+//
+//  Created by luyao ma on 4/18/15.
+//  Copyright (c) 2015 luyao ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
